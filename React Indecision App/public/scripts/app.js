34198929c43dfde525ce10f91a8493f7d6a519a8
Katherine-Kumar <Katherine2bk@gmail.com>
2020-06-02 15:49:01 -0700

    tested dynamic objects, continuing to slowly build

--- React Indecision App/public/scripts/app.js
+++ React Indecision App/public/scripts/app.js
@@ -1,10 +1,45 @@
 "use strict";
 
-var template = React.createElement(
-  "p",
-  null,
-  "This is JSX!"
-);
 var appRoot = document.getElementById("app");
 
+var app = {
+    title: 'Schedule App',
+    subtitle: "What's your goal for today?"
+};
+
+var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.subtitle
+    )
+);
+
 ReactDOM.render(template, appRoot);
+
+var user = {
+    name: 'Andrew',
+    age: 27,
+    location: 'New York'
+};
+
+// var userName = 'Mike';
+// var userAge = 27;
+// var userLocation = 'New York';
+
+// var templateTwo = (
+//     <div>
+//         <h1>{user.name}</h1>
+//         <p>Age: {user.age}</p>
+//         <p>Location: {user.location}</p>
+//     </div>
+// );
+
+// ReactDOM.render(templateTwo, appRoot);
--- React Indecision App/src/app.js
+++ React Indecision App/src/app.js
@@ -1,4 +1,36 @@
-var template = <p>This is JSX!</p>
+
 var appRoot = document.getElementById("app");
 
+var app = {
+    title: 'Schedule App',
+    subtitle: "What's your goal for today?"
+}
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+    </div>
+);
+
 ReactDOM.render(template, appRoot);
+
+var user = {
+    name: 'Andrew',
+    age: 27,
+    location: 'New York'
+};
+
+// var userName = 'Mike';
+// var userAge = 27;
+// var userLocation = 'New York';
+
+// var templateTwo = (
+//     <div>
+//         <h1>{user.name}</h1>
+//         <p>Age: {user.age}</p>
+//         <p>Location: {user.location}</p>
+//     </div>
+// );
+
+// ReactDOM.render(templateTwo, appRoot);
