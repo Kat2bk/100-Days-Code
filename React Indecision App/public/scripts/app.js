b4d39b406785a287adc11aa6c7a9ee45f6029d79
Katherine-Kumar <Katherine2bk@gmail.com>
2020-06-01 19:33:00 -0700

    added babel dependencies, created src file, JSX is displaying now

--- React Indecision App/public/scripts/app.js
+++ React Indecision App/public/scripts/app.js
@@ -1,4 +1,10 @@
-var template = <p>This is JSX from app.js!</p>
+"use strict";
+
+var template = React.createElement(
+  "p",
+  null,
+  "This is JSX!"
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
