9c104610cf2bdbfbac6c79eeb7a16d38937060bc
unknown <Katherine2bk@gmail.com>
2020-06-24 13:26:43 -0700

    added babel dependencies, now have package.json and package-lock, node_modules

---
+++ Indescision App/public/scripts/app.js
@@ -0,0 +1,7 @@
+// JSX - JavaScript XML
+
+var template = <p>This is JSX element</p>
+
+var appRoot = document.getElementById(app);
+ReactDOM.render(template, appRoot);
+// takes two arguements, the JSX you want to render and the DOM element you want to render to
