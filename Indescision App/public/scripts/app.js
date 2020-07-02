844f97b6116abef55a226245526944b07fc058a6
unknown <Katherine2bk@gmail.com>
2020-07-02 13:33:32 -0700

    testing manual re-render state function

--- Indescision App/public/scripts/app.js
+++ Indescision App/public/scripts/app.js
@@ -5,48 +5,87 @@
 var appName = {
     title: "Indescision App",
     subtitle: "Make things easy on you",
-    options: ['One', 'Two']
+    options: []
 };
 
-var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        appName.title
-    ),
-    appName.subtitle && React.createElement(
-        "p",
-        null,
-        appName.subtitle
-    ),
-    appName.options.length > 0 ? "Here are your options" : "No options",
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            "Item One"
-        ),
-        React.createElement(
-            "li",
-            null,
-            "Item Two"
-        )
-    ),
-    React.createElement(
-        "form",
-        null,
-        React.createElement("input", { type: "text", name: "option" }),
-        React.createElement(
-            "button",
-            null,
-            "Add Option"
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(event) {
+    event.preventDefault();
+    var option = event.target.elements.option.value;
+    if (option) {
+        appName.options.push(option);
+        event.target.elements.option.value = '';
+    }
+    renderFormApp();
+};
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+var renderFormApp = function renderFormApp() {
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            appName.title
+        ),
+        appName.subtitle && React.createElement(
+            "p",
+            null,
+            appName.subtitle
+        ),
+        appName.options.length > 0 ? "Here are your options" : "No options",
+        React.createElement(
+            "p",
+            null,
+            appName.options.length
+        ),
+        React.createElement(
+            "ol",
+            null,
+            React.createElement(
+                "li",
+                null,
+                "Item One"
+            ),
+            React.createElement(
+                "li",
+                null,
+                "Item Two"
+            )
+        ),
+        React.createElement(
+            "form",
+            { onSubmit: onFormSubmit },
+            React.createElement("input", { type: "text", name: "option" }),
+            React.createElement(
+                "button",
+                null,
+                "Add Option"
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderFormApp();
+
+// var template = (
+//     <div>
+//     <h1>{appName.title}</h1>
+//     {(appName.subtitle && <p>{appName.subtitle}</p> )}
+//     {appName.options.length > 0 ? "Here are your options" : "No options" }
+//     <p>{appName.options.length}</p>
+//     <ol>
+//     <li>Item One</li>
+//     <li>Item Two</li>
+//     </ol>
+//     <form onSubmit={onFormSubmit}>
+//     <input type="text" name="option" />
+//     <button>Add Option</button>
+//     </form>
+//     </div>
+// )
+
+// const appRoot = document.getElementById("app");
+// ReactDOM.render(template, appRoot);
--- Indescision App/src/app.js
+++ Indescision App/src/app.js
@@ -3,24 +3,59 @@
 const appName = {
     title: "Indescision App",
     subtitle: "Make things easy on you",
-    options: ['One', 'Two']
+    options: []
 }
 
-var template = (
-    <div>
-    <h1>{appName.title}</h1>
-    {(appName.subtitle && <p>{appName.subtitle}</p> )}
-    {appName.options.length > 0 ? "Here are your options" : "No options" }
-    <ol>
-    <li>Item One</li>
-    <li>Item Two</li>
-    </ol>
-    <form>
-    <input type="text" name="option" />
-    <button>Add Option</button>
-    </form>
-    </div>
-)
+const onFormSubmit = (event) => {
+    event.preventDefault();
+    const option = event.target.elements.option.value;
+    if (option) {
+        appName.options.push(option);
+        event.target.elements.option.value = '';
+    }
+    renderFormApp();
+}
 
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+const renderFormApp = () => {
+    var template = (
+        <div>
+        <h1>{appName.title}</h1>
+        {(appName.subtitle && <p>{appName.subtitle}</p> )}
+        {appName.options.length > 0 ? "Here are your options" : "No options" }
+        <p>{appName.options.length}</p>
+        <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+        </ol>
+        <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+        </form>
+        </div>
+    )
+    ReactDOM.render(template, appRoot);
+}
+
+renderFormApp();
+
+// var template = (
+//     <div>
+//     <h1>{appName.title}</h1>
+//     {(appName.subtitle && <p>{appName.subtitle}</p> )}
+//     {appName.options.length > 0 ? "Here are your options" : "No options" }
+//     <p>{appName.options.length}</p>
+//     <ol>
+//     <li>Item One</li>
+//     <li>Item Two</li>
+//     </ol>
+//     <form onSubmit={onFormSubmit}>
+//     <input type="text" name="option" />
+//     <button>Add Option</button>
+//     </form>
+//     </div>
+// )
+
+// const appRoot = document.getElementById("app");
+// ReactDOM.render(template, appRoot);
