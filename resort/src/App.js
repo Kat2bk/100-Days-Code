b830d1e197807290cb8830912b467ffec5f972bf
unknown <Katherine2bk@gmail.com>
2020-07-25 09:54:34 -0700

    imported Route and Switch into App.js, set up Routes with params for single rooms

--- resort/src/App.js
+++ resort/src/App.js
@@ -4,18 +4,20 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import Room from "./pages/Room";
 import Error from "./pages/Error";
+import {Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div>
-      <Home />
-      <Rooms />
-      <Room />
-      <Error />
-      </div>
+      <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/rooms" component={Rooms} />
+      <Route path="/rooms/:slug" component={Room} />
+      </Switch>
     </div>
   );
 }
+
+// we need to set up route params because just using path="single-room" will just access a single single-room page, we want different information for every single room. we do that by using colon : and then the variable we want to call the room "slug" is a stand-in for that info
 
 export default App;
