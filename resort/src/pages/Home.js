98e1affd273dd51a57be1688d18186e73b1a599a
unknown <Katherine2bk@gmail.com>
2020-07-26 10:35:26 -0700

    added a Hero component, learned a way of passing deconstructed props down into className to change it... imported Hero into Home page, added hero props, set up defaultProps

--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Hero from "../components/Hero";
 
 const Home = () => {
     return (
-        <div>
-        <p>Hello from Home page</p>
-        </div>
+        <Hero hero="defaultHero" />
     )
 }
 
 export default Home;
+
+// we are adding the className like props to reuse and change it... there are two of them... .defaultHero and .roomsHero
+
+// we want to add defaultProps in Hero component
