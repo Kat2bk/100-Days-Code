347e9b024795ea5bbbfd8cbaea9c5c1cd4829f6f
unknown <Katherine2bk@gmail.com>
2020-07-27 13:24:18 -0700

    set up basic Context Api in Context.js...imported into index.js, wrapped it around Router and App

--- resort/src/components/Services.js
+++ resort/src/components/Services.js
@@ -1,13 +1,47 @@
 import React from "react";
 import Title from "./Title";
+import {FaCocktail, FaHiking, FaBeer, FaShuttleVan } from "react-icons/fa";
 
 class Services extends React.Component {
+    state={
+        services: [
+            {
+                icon: <FaCocktail />,
+                title: "Free Cocktails",
+                info: "Lorem gubergren duo vero invidunt eos et. Tempor sadipscing magna diam gubergren sit eos. Sit labore et voluptua sadipscing ipsum."
+            },
+            {
+                icon: <FaHiking />,
+                title: "Endless Hiking",
+                info: "Lorem gubergren duo vero invidunt eos et. Tempor sadipscing magna diam gubergren sit eos. Sit labore et voluptua sadipscing ipsum."
+            },
+            {
+                icon: <FaShuttleVan />,
+                title: "Free Shuttle",
+                info: "Lorem gubergren duo vero invidunt eos et. Tempor sadipscing magna diam gubergren sit eos. Sit labore et voluptua sadipscing ipsum."
+            },
+            {
+                icon: <FaBeer />,
+                title: "Strongest Beer",
+                info: "Lorem gubergren duo vero invidunt eos et. Tempor sadipscing magna diam gubergren sit eos. Sit labore et voluptua sadipscing ipsum."
+            }
+        ]
+    }
 
     render() {
         return (
-            <div>
+            <section className="services">
             <Title title="services" />
+            <div className="services-center">
+            {this.state.services.map((item, index) => {
+                return <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+                </article>
+            })}
             </div>
+            </section>
         )
     }
 
