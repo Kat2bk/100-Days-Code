30e25e6e8a5d442a502ef6bad389c5801aee8e5f
unknown <Katherine2bk@gmail.com>
2020-07-26 19:45:22 -0700

    did the same thing for Rooms page as Home and Error with Banner component, created two new component files... Title and Services

---
+++ resort/src/components/Services.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Services extends React.Component {
+
+    render() {
+        return (
+            <div>
+            Hello from services
+            </div>
+        )
+    }
+
+}
+
+export default Services;
