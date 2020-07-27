bb16a0b015dbd31fb41452cd1b8280567381a86d
unknown <Katherine2bk@gmail.com>
2020-07-26 19:22:07 -0700

    set up Banner component, deconstructed props for title, subtitle and children... imported Banner into Home page, inserted it in-between Hero component, passing props to it

--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom";
 
 const Home = () => {
     return (
-        <Hero hero="defaultHero" />
+        <Hero hero="defaultHero">
+        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">
+        Our Rooms
+        </Link>
+        </Banner>
+        </Hero>
     )
 }
 
 export default Home;
 
-// we are adding the className like props to reuse and change it... there are two of them... .defaultHero and .roomsHero
+// we are adding the className like props to reuse and change it. there are two of them: .defaultHero and .roomsHero
 
 // we want to add defaultProps in Hero component
+
+// we are importing Banner, and moving the children props inside the Hero component. we are giving the props to the title and subtitle
+
+// we are importing Link for the button to route to /rooms
