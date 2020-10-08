e20af68fa02a2edf4562e82b150e44384bd85f60
unknown <Katherine2bk@gmail.com>
2020-10-08 14:03:10 -0700

    set up mapStateToProps and connect to ExpenseList and passed everything to Expense Dashboard

--- budget-app/src/index.js
+++ budget-app/src/index.js
@@ -11,8 +11,6 @@ import rootReducer from "./reducers";
 
 const store = createStore(rootReducer);
 
-console.log(store.getState());
-
 ReactDOM.render(
   <Provider store={store}>
   <Router><App /></Router></Provider>,
