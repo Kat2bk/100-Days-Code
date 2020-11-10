68a5068f1268d51960d78ffc3aeb38ed59f3ba25
unknown <Katherine2bk@gmail.com>
2020-11-10 12:16:59 -0800

    set up an ajax fetch to get data, pushing it using the spread operator into an empty array called cities. created a filter function using the match method which involves a regular expression that looks globally for uppercase or lowercase letters. created two querySelectors and two event listeners that involve the search bar and an unordered list. created a second function that uses the filter fuction to display the data using this.value, and the data array

--- JS-SearchJSON/script.js
+++ JS-SearchJSON/script.js
@@ -1,1 +1,39 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+    .then(response => response.json())
+    // spread operator
+    .then(data => cities.push(...data))
+
+function findCities(wordToMatch, cities) {
+    return cities.filter(place => {
+        // we filter through the array of the city property
+        // we need to match what we search
+        // so we will use regex
+        // we cannot put wordToMatch inside, we must
+        //create a regular expression outside
+        const regex = new RegExp(wordToMatch, 'gi');
+        // check for city or state
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
+
+function displayMatches() {
+    const matchData = findCities(this.value, cities)
+    console.log(matchData);
+}
+
+const searchInput = document.querySelector('.search-bar');
+
+const matches = document.querySelector('.filter');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+// regex is short for regular expression:
+// A regular expression is a sequence of characters 
+// that define a search pattern. Usually such patterns
+// are used by string-searching algorithms for "find" 
+// or "find and replace" operations on strings, or for input validation.
